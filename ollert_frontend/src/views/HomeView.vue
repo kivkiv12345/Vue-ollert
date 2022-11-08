a65b7dcb6ef0059d4834fe5dc99e94b801fecaba
Gustav Thoft <35370673+TotallyNotRust@users.noreply.github.com>
2022-11-08 13:40:15 +0100

    working on getting card list

--- ollert_frontend/src/views/HomeView.vue
+++ ollert_frontend/src/views/HomeView.vue
@@ -1,7 +1,19 @@
-<script setup>
+<script>
+var cardLists = [];
+export default {
 
+	async getCardLists() {
+		var raw_lists = await fetch("http://localhost:8000/api/cardlist-list/?format=json");
+
+		console.log(raw_lists);
+	},
+	data() {
+		getCardLists();
+		return {}
+	}
+}
 </script>
 
 <template>
-  
+
 </template>
